3173afde3cb451594be8632cf8c8d0f11174f519
Nguyen Duc Nam <net12k44@gmail.com>
2016-12-23 14:41:59 +0700

    Update code

--- HumanDetection/peopledetection.cpp
+++ HumanDetection/peopledetection.cpp
@@ -1,55 +1,48 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <ctime>
+#include "config.h"
 
 using namespace std;
 using namespace cv;
+
+Config conf;
 
 double getTime(clock_t start, clock_t end) {
 	return double(end - start) / CLOCKS_PER_SEC;
 }
 
-const int WIDTH = 240;
-const int HEIGHT = 180;
 
 int main(int argc, const char * argv[]) {
 	VideoCapture cap;
-	cap.open("C:\\Users\\XuanDuc\\Desktop\\VIDEO0042.mp4");
-	cout << cap.get(CV_CAP_PROP_FRAME_WIDTH); cout << " "; cout << cap.get(CV_CAP_PROP_FRAME_HEIGHT); cout << endl;
-	cap.set(CV_CAP_PROP_FRAME_WIDTH , 320);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT , 240);
-	cout << cap.get(CV_CAP_PROP_FRAME_WIDTH); cout << " "; cout << cap.get(CV_CAP_PROP_FRAME_HEIGHT); cout << endl;
+	//cap.open("C:\\Users\\XuanDuc\\Desktop\\VIDEO0042.mp4");
+	cap.open(conf.getVideo());	
+	cout << cap.get(CV_CAP_PROP_FPS) << endl;
+	cap.set(CV_CAP_PROP_FRAME_WIDTH , conf.getWidth());
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT , conf.getHeight());
 	if (!cap.isOpened())
 		return -1;
 
 	Mat img;
-	HOGDescriptor hog;
-	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+	Detector* detector = conf.getDetector();
 
 	namedWindow("video capture", CV_WINDOW_AUTOSIZE);
 	while (true) {
 		clock_t start = clock();
-		cap >> img;
+		bool ok = cap.read(img);
+		if (!ok)
+			break;
 	
-		resize(img, img, Size(WIDTH, HEIGHT), 0, 0, INTER_CUBIC);
+		resize(img, img, Size(conf.getWidth(), conf.getHeight()), 0, 0, INTER_CUBIC);
 		cout << img.cols << " " << img.rows << endl;
 		if (!img.data)
 			continue;
 
-		vector<Rect> found, found_filtered;
-		hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2);
-
-		size_t i, j;
-		for (i = 0; i < found.size(); i++) {
-			Rect r = found[i];
-			for (j = 0; j < found.size(); j++)
-				if (j != i && (r & found[j]) == r)
-					break;
-			if (j == found.size())
-				found_filtered.push_back(r);
-		}
-		for (i = 0; i < found_filtered.size(); i++) {
+		vector<Rect> found_filtered = detector->detect(img);
+		
+		for (size_t i = 0; i < found_filtered.size(); i++) {			
 			Rect r = found_filtered[i];
+			cout << r.tl() << " " << r.br() << endl;
 			r.x += cvRound(r.width * 0.1);
 			r.width = cvRound(r.width * 0.8);
 			r.y += cvRound(r.height * 0.06);
@@ -65,5 +58,6 @@ int main(int argc, const char * argv[]) {
 
 
 	}
+	delete detector;
 	return 0;
 }
